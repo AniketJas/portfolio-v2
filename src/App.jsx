56ed7da33072b2ef9b2626c84a74adf3a55fc587
Aniket Jas <aniket.jas20@gmail.com>
2025-12-16 19:13:44 +0530

    refactored code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 import { useEffect } from "react";
-import SideNavbar from "./components/SideNavbar"
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Skills from "./pages/Skills";
-import Experience from "./pages/Experience";
-import Education from "./pages/Education";
-import Certificates from "./pages/Certificates";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
+import Layout from "./Layout"
 
 const App = () => {
 
@@ -17,32 +9,7 @@ const App = () => {
 
   return (
     <>
-      <SideNavbar />
-
-      <section id="home">
-        <Home />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="skills">
-        <Skills />
-      </section>
-      <section id="experience">
-        <Experience />
-      </section>
-      <section id="education">
-        <Education />
-      </section>
-      <section id="certificates">
-        <Certificates />
-      </section>
-      <section id="projects">
-        <Projects />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
+      <Layout />
     </>
   )
 }
