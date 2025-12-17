dedc2a392ad63e46073eff18a7670a483d5f2b2e
Aniket Jas <aniket.jas20@gmail.com>
2025-12-17 16:10:35 +0530

    changes for responsive layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Links from '../components/Links'
 
 const Home = () => {
   return (
-    <div className='relative w-full md:h-screen overflow-hidden flex items-center justify-center md:justify-start'>
+    <div className='relative w-full lg:h-screen overflow-hidden flex items-center justify-center md:justify-start'>
       <picture className='absolute inset-0 -z-10'>
         <source srcSet={bg} type="image/webp" />
         <img src={bg} alt="Background" className='w-full h-full object-cover' />
@@ -11,14 +11,14 @@ const Home = () => {
 
       <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-black/30 md:to-transparent" />
 
-      <div className="relative z-10 w-full px-6 md:px-32 pt-20 pb-16 text-center md:text-left">
-        <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold text-white drop-shadow-lg opacity-0 mb-3 md:mb-4 translate-y-4 animate-fade-in">
+      <div className="relative z-10 w-full px-6 md:px-20 lg:px-32 pt-20 md:pt-32 pb-16 md:pb-28 text-center md:text-left">
+        <h1 className="text-4xl md:text-5xl lg:text-8xl font-extrabold text-white drop-shadow-lg opacity-0 mb-3 md:mb-4 translate-y-4 animate-fade-in">
           Hi, I'm Aniket Jas
         </h1>
-        <p className="text-sm sm:text-base md:text-2xl font-semibold text-gray-100 mb-2 md:mb-4 opacity-0 translate-y-4 animate-fade-in delay-150">
+        <p className="text-sm sm:text-base lg:text-2xl font-semibold text-gray-100 mb-2 md:mb-4 opacity-0 translate-y-4 animate-fade-in delay-150">
           Software Engineer | Full-stack Developer
         </p>
-        <p className="text-sm sm:text-base md:text-2xl text-gray-200 mb-4 translate-y-4 opacity-0 animate-fade-in delay-300">
+        <p className="text-sm sm:text-base lg:text-2xl text-gray-200 mb-4 translate-y-4 opacity-0 animate-fade-in delay-300">
           I make imaginations come to life through code
         </p>
 
